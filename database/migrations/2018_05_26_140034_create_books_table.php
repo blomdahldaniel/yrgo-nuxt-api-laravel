989235e8e11457f44ace6cb12121b51b8bdc7bdb
blomdahldaniel <blomdahl.daniel@gmail.com>
2018-05-29 09:49:54 +0200

    ta bort foreign keys tmep

--- database/migrations/2018_05_26_140034_create_books_table.php
+++ database/migrations/2018_05_26_140034_create_books_table.php
@@ -16,9 +16,6 @@ class CreateBooksTable extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')
-              ->references('id')->on('users')
-              ->onDelete('cascade');
             $table->string('title')->nullable()->default(null);
             $table->string('author')->nullable()->default(null);
             $table->text('image_path')->nullable()->default(null);
